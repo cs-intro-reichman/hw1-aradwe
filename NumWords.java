c3e5ff390c83983855d59f0d52ec206ef8306978
Arad Weinstein <aradwe@gmail.com>
2024-11-05 15:37:09 +0200

    change var names + fix indents

--- NumWords.java
+++ NumWords.java
@@ -1,11 +1,11 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    int input_num = Integer.parseInt(args[0]);
+		int inputNum = Integer.parseInt(args[0]);
 	   
-	   int hundreds = input_num / 100;
-	   int tens = (input_num % 100) / 10;
-	   int ones = input_num % 10;
+		int hundreds = inputNum / 100;
+	    int tens = (inputNum % 100) / 10;
+	    int ones = inputNum % 10;
 	   
 		System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.");
 	}
