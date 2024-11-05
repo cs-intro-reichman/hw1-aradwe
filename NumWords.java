b3eb23092836145a810a51ace446fd01cca27f7a
Arad Weinstein <aradwe@gmail.com>
2024-11-05 15:39:11 +0200

    add comments

--- NumWords.java
+++ NumWords.java
@@ -2,7 +2,8 @@
 public class NumWords {
 	public static void main(String args[]) {
 		int inputNum = Integer.parseInt(args[0]);
-	   
+
+		// parse each digit form the user input 
 		int hundreds = inputNum / 100;
 	    int tens = (inputNum % 100) / 10;
 	    int ones = inputNum % 10;
